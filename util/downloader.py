9b12431b0c0bf7d4b34b1249bec669d40552c05a
yangm11 <mingzhang.yang@nih.gov>
2017-10-06 15:20:31 -0400

    fjios

--- util/downloader.py
+++ util/downloader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import urllib.request
 import sys
 
@@ -16,3 +17,6 @@ def download(arr):
         file_name = url.split('/')[-1]
         urllib.request.urlretrieve(url, file_name)
     print("Download completed!")
+
+if __name__ == '__main__':
+    print('Hello World')
