d0f6beae259b4bdd4d8724caab4ec92c8817c6d9
yangm11 <mingzhang.yang@nih.gov>
2017-10-06 15:17:44 -0400

    download

---
+++ util/downloader.py
@@ -0,0 +1,18 @@
+import urllib.request
+import sys
+
+def parseFile(path):
+    urls = []
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+            if line[:4] == 'http':
+                urls.append(line)
+    return urls
+
+
+def download(arr):
+    for url in arr:
+        file_name = url.split('/')[-1]
+        urllib.request.urlretrieve(url, file_name)
+    print("Download completed!")
